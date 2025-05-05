cbf277379ee10f6cc56c770dc0973aed51ffeadc
AQodS <qoddri@gmail.com>
2025-05-06 02:11:39 +0700

    feat: add cors

--- routes/routes.go
+++ routes/routes.go
@@ -4,11 +4,20 @@ import (
 	"backend-go/controllers"
 	"backend-go/middlewares"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:  []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+	}))
+
 	router.POST("/api/register", controllers.Register)
 	router.POST("/api/login", controllers.Login)
 	router.GET("/api/users", middlewares.AuthMiddleware(), controllers.FindUsers)
